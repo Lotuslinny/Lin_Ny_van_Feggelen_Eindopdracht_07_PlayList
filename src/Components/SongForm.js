bc90935400775b3f30c9cce99504db90fb4ba407
Lotuslinny <linny@fegg.nl>
2021-02-22 15:01:07 +0100

    hoi alert

--- src/Components/SongForm.js
+++ src/Components/SongForm.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const SongForm = () => {
-  <div>
-
-  </div>
+const SongForm = (props) => {
+  return (
+    <div>
+      <form /* onSubmit={props.handleSubmit */>
+        <button onClick={props.addSong}>Add Song</button>
+      </form>
+    </div >
+  )
 }
 
 export default SongForm;
