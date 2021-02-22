f7a995f1e48d085fb027b0ac483de3e7fb1fc04a
Lotuslinny <linny@fegg.nl>
2021-02-22 09:30:11 +0100

    ff afspraak

---
+++ src/Components/SongForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SongForm = () => {
+  <div>
+
+  </div>
+}
+
+export default SongForm;
---
+++ src/SongOverview.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import SongForm from "./Components/SongForm";
+
+class SongOverview extends Component {
+
+  constructor() {
+    super()
+    this.state =
+    {
+      songs: []
+    }
+
+  }
+
+  addSong = (song) => {
+    // doe iets om de state aan te passen
+  }
+
+  render() {
+    return (
+      <div>
+        <SongForm addSong={this.addSong} />
+        <table style="width:100%">
+          <tr className="song-header">
+            <th className="song-row__item">Song</th>
+            <th className="song-row__item">Artist</th>
+            <th className="song-row__item">Genre</th>
+            <th className="song-row__item">Rating</th>
+          </tr>
+        </table>
+        {/* <SongList songs={this.state.songs} /> */}
+      </div>
+    );
+  }
+}
+
+export default SongOverview;
