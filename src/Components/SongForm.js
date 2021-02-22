00ac16ce3443930d4e65234f051b8dd1e52d9b55
Lotuslinny <linny@fegg.nl>
2021-02-22 19:11:30 +0100

    WHY

--- src/Components/SongForm.js
+++ src/Components/SongForm.js
@@ -3,8 +3,41 @@ import React from "react";
 const SongForm = (props) => {
   return (
     <div>
-      <form /* onSubmit={props.handleSubmit */>
-        <button onClick={props.addSong}>Add Song</button>
+      <form onSubmit={props.addSong}>
+        <table style={{ width: "100%" }}>
+          <tbody>
+            <tr className="song-header">
+              <th className="song-row__item">
+                <input
+                  className="input"
+                  type={props.text}
+                  value={props.value}
+                  onChange={props.handleOnChange}></input></th>
+              <th className="song-row__item">
+                <input
+                  className="input"
+                  type={props.text}
+                  value={props.value}
+                  onChange={props.handleOnChange}></input></th>
+              <th className="song-row__item">
+                <input
+                  className="input"
+                  type={props.text}
+                  value={props.value}
+                  onChange={props.handleOnChange}></input></th>
+              <th className="song-row__item">
+                <input
+                  className="input"
+                  type={props.text}
+                  value={props.value}
+                  onChange={props.handleOnChange}></input></th>
+            </tr>
+          </tbody>
+        </table >
+        <input className="input__submit"
+          type="submit"
+          value="Add Song">
+        </input>
       </form>
     </div >
   )
--- src/SongOverview.js
+++ src/SongOverview.js
@@ -3,55 +3,52 @@ import SongForm from "./Components/SongForm";
 
 class SongOverview extends Component {
 
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
     this.state =
     {
-      songs: []
+      songs: [],
+
     }
     this.addSong = this.addSong.bind(this);
+    this.handleOnChange = this.handleOnChange.bind(this);
   }
   addSong = () => {
-    alert("hoi")
-
-
-    // doe iets om de state aan te passen. 
-
-    /*
-    1. Kijk eens wat in de parameter song staat (console.log)
-    2. Kun je daar meteen wat mee, of moet je iets doen om het leesbaar te maken?
-    3. Zodra je toegang hebt tot de stukjes uit song (Song, Artist, Genre, Rating)
-    kun je iets met deze stukjes doen. Individueel, of als groep. (console.log? ;) )
-    4. Zou je de stukjes in een bepaalde volgorde in een nieuwe regel in een state 
-    kunnen zetten?
-    5. Kun je deze state dan ook weer uitlezen? (console.log)
-    6. Zodra dat is gelukt, kijk eens of je ook nog regel kan 
-    toevoegen (hint, nog een keer op de knop add song drukken)
-    7. Kun je nu ook de toegevoegde regel zien in state?
-    8. Love you, je kan het!
-
-    p.s. Ben je hierboven eigenlijk niet al stiekem de song list aan het maken, 
-    waar songs in staan? En is de volgende stap niet dat je waardes uit de state 
-    moet weergeven in een list/table view? 
-    De ene component voegt wat toe aan de state, de andere leest em uit. 
-    */
+    console.log("hoi")
   }
+  handleOnChange = () => {
+    console.log("oh nee")
+  }
+
+  // doe iets om de state aan te passen. 
+
+  /*
+  1. Kijk eens wat in de parameter song staat (console.log)
+  2. Kun je daar meteen wat mee, of moet je iets doen om het leesbaar te maken?
+  3. Zodra je toegang hebt tot de stukjes uit song (Song, Artist, Genre, Rating)
+  kun je iets met deze stukjes doen. Individueel, of als groep. (console.log? ;) )
+  4. Zou je de stukjes in een bepaalde volgorde in een nieuwe regel in een state 
+  kunnen zetten?
+  5. Kun je deze state dan ook weer uitlezen? (console.log)
+  6. Zodra dat is gelukt, kijk eens of je ook nog regel kan 
+  toevoegen (hint, nog een keer op de knop add song drukken)
+  7. Kun je nu ook de toegevoegde regel zien in state?
+  8. Love you, je kan het!
+
+  p.s. Ben je hierboven eigenlijk niet al stiekem de song list aan het maken, 
+  waar songs in staan? En is de volgende stap niet dat je waardes uit de state 
+  moet weergeven in een list/table view? 
+  De ene component voegt wat toe aan de state, de andere leest em uit. 
+  */
+
 
   render() {
     return (
       <div>
-        <SongForm addSong={this.addSong}
-         /* onSubmit={props.handleSubmit}  */ />
-        <table style={{ width: "100%" }}>
-          <tbody>
-            <tr className="song-header">
-              <th className="song-row__item" field="Hello">Song</th>
-              <th className="song-row__item">Artist</th>
-              <th className="song-row__item">Genre</th>
-              <th className="song-row__item">Rating</th>
-            </tr>
-          </tbody>
-        </table>
+        <SongForm
+          addSong={this.addSong}
+          handleOnChange={this.handleOnChange}
+        />
         {/* <SongList songs={this.state.songs} /> */}
       </div >
     );
